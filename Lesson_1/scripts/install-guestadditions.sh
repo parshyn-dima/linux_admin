67cc06aea8539d94f4214d3d880131b3d6e1719e
Dmitriy Parshin <parshyn.dima@gmail.com>
2020-02-02 18:16:04 +0300

    Edit files

--- Lesson_1/scripts/install-guestadditions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo mkdir /media/VirtualBoxGuestAdditions
-sudo mount -t iso9660 -o loop /home/vagrant/VBoxGuestAdditions.iso /media/VirtualBoxGuestAdditions
-sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum install -y gcc kernel-devel kernel-headers dkms make bzip2 perl
-sudo cd /media/VirtualBoxGuestAdditions
-sudo ./VBoxLinuxAdditions.run
-sudo reboot
--- Lesson_1/scripts/kernel-update.sh
+++ Lesson_1/scripts/kernel-update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-sudo yum install -y http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
-sudo yum --enablerepo elrepo-kernel install kernel-ml -y
+sudo yum -y install https://www.elrepo.org/elrepo-release-7.0-4.el7.elrepo.noarch.rpm
+sudo rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+sudo yum --enablerepo=elrepo-kernel install kernel-ml -y
+sudo yum -y --enablerepo=elrepo-kernel install kernel-ml-{devel,headers,perf}
 sudo grub2-mkconfig -o /boot/grub2/grub.cfg
 sudo grub2-set-default 0
 sudo reboot
