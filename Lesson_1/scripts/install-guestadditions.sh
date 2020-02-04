399e09e49a46bbdf2cc25ca6e93fde125d74aeac
Dmitriy Parshin <parshyn.dima@gmail.com>
2020-02-04 12:53:30 +0300

    Edite files

--- Lesson_1/scripts/install-guestadditions.sh
+++ Lesson_1/scripts/install-guestadditions.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo yum -y install https://www.elrepo.org/elrepo-release-7.0-4.el7.elrepo.noarch.rpm
 sudo rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-#sudo yum -y update kernel*
 sudo yum -y --enablerepo=elrepo-kernel install --skip-broken kernel-ml-{devel,headers} perf
 sudo mkdir /media/VirtualBoxGuestAdditions
 sudo mount -t iso9660 -o loop /home/vagrant/VBoxGuestAdditions.iso /media/VirtualBoxGuestAdditions
