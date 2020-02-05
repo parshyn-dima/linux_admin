ad268a4f8f9290de55d2638fc7ee386db9c6e51b
Dmitriy Parshin <parshyn.dima@gmail.com>
2020-02-05 15:47:03 +0300

    Edite files

--- Lesson_1/scripts/kernel-update.sh
+++ Lesson_1/scripts/kernel-update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo yum -y install https://www.elrepo.org/elrepo-release-7.0-4.el7.elrepo.noarch.rpm
-sudo rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 sudo yum --enablerepo=elrepo-kernel install kernel-ml -y
 sudo grub2-mkconfig -o /boot/grub2/grub.cfg
 sudo grub2-set-default 0
