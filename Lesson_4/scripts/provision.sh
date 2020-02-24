ececb5c10f18863c64a68df6bbe84a695842fe74
dparshin <parshyn.dima@gmail.com>
2020-02-24 12:14:54 +0300

    Add files for Lesson_4

--- Lesson_4/scripts/provision.sh
+++ Lesson_4/scripts/provision.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 yum install -y mailx
 mkdir /var/log/nginx
-wget 
+wget https://github.com/parshyn-dima/linux_admin/blob/master/Lesson_4/logs/access-otus.log
 sed -i "s|[0-9][0-9]/Aug|$to_date|g" access-otus.log
 mkdir /vagrant/script/
-wget
-wget
+wget https://github.com/parshyn-dima/linux_admin/blob/master/Lesson_4/scripts/nginx_parser.sh
+wget https://github.com/parshyn-dima/linux_admin/blob/master/Lesson_4/scripts/send_mail_logs.sh
 cp /vagrant/script/send_mail_logs.sh /etc/cron.horly/
