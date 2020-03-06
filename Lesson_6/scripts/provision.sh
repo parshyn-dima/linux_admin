0bbe8cbe6bd94130b26ca1751d9fa74d78c5ba5c
dparshin <parshyn.dima@gmail.com>
2020-03-06 18:41:12 +0300

    Add files for Lesson_6

---
+++ Lesson_6/scripts/provision.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir /usr/lib/dracut/modules.d/01test
+cp /vagrant/scripts/module-setup.sh /usr/lib/dracut/modules.d/01test/
+cp /vagrant/scripts/test.sh /usr/lib/dracut/modules.d/01test/
+mkinitrd -f -v /boot/initramfs-$(uname -r).img $(uname -r)
+grubby --remove-args="rhgb quiet"
