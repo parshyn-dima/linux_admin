c0f3042cb3dd6dc197f5f9c42de6411945c43b73
Dmitriy Parshin <parshyn.dima@gmail.com>
2020-06-07 12:36:34 +0300

    Add files

--- Lesson_20/scripts/inetRouter2.sh
+++ Lesson_20/scripts/inetRouter2.sh
@@ -7,5 +7,9 @@ sudo sysctl -p
 fi
 sudo nmcli con mod "System eth1" +ipv4.routes "192.168.0.0/16 192.168.252.1"
 sudo nmcli con up "System eth1"
+sudo nmcli con mod "System eth1" ipv4.gateway 192.168.252.1 ipv4.dns 8.8.8.8
+sudo nmcli con up "System eth1"
+sudo nmcli con mod "System eth0" ipv4.never-default yes
+sudo nmcli con up "System eth0"
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination 192.168.0.2:8080
 sudo iptables -t nat -A POSTROUTING --destination 192.168.0.2/16 -j SNAT --to-source 192.168.252.2
